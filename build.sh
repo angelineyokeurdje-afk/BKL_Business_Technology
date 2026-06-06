024864c5c07621ae4c78518f72933a684e59b71f
angeline <angelineyokeurdje@gmail.com>
2026-06-06 12:27:20 +0100

    Fix superuser creation

--- build.sh
+++ build.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-pip install "whitenoise>=6.0.0,<7.0.0"
 
-# 1. Installation des dépendances avec le bon 's'
+# 1. Installation des dépendances
 pip install -r requirements.txt
 
-# 2. Collecte des fichiers statiques pour réparer le design blanc
+# 2. Collecte des fichiers statiques
 python manage.py collectstatic --no-input
 
-# 3. Application des migrations de la base de données
+# 3. Application des migrations
 python manage.py migrate
 
 # 4. Création sécurisée du superutilisateur sans doublon
 python manage.py shell -c "
 from django.contrib.auth import get_user_model;
 User = get_user_model();
-if not User.objects.filter(username='admin').exists():
+if not User.objects.filter(username='BRUNO').exists():
     User.objects.create_superuser('BRUNO', 'bklbusinesstechnologies@gmail.com', 'bkl@-25')
     print('Superutilisateur créé avec succès !')
 else:
     print('Le superutilisateur existe déjà.')
 "
-#Relance forcee
