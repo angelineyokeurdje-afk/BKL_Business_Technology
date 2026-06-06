e053b8d4296d76ae390a81e3204f6dfa03600dd6
angeline <angelineyokeurdje@gmail.com>
2026-06-06 08:08:14 +0100

    verification du s a requirements dans build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
-
-# 1. Installer les dépendances
 pip install -r requirements.txt
-
-# 2. Appliquer les migrations
 python manage.py migrate
-
-# 3. Créer le superutilisateur automatiquement s'il n'existe pas
 python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('BRUNO', 'bklbusinesstechnologies@gmail.com', 'bklb@-25')"
